27aa3288feadc710036a7f7d9f72a1766acda9db
Andy Lun <andylun1206@gmail.com>
2019-03-13 05:00:36 -0500

    Completed tutorial with randomly generated game objects

---
+++ Assets/Scripts/PickupController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickupController : MonoBehaviour
+{
+    public int minNumObjects;
+    public int maxNumObjects;
+    public Transform pickupPrefab;
+
+    public GameObject ground;
+    public float wallOffset;
+
+    public Text remainingPickupText;
+
+    private int numObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Random rng = new System.Random();
+
+        numObjects = rng.Next(minNumObjects, maxNumObjects);
+        Vector3 bounds = ground.GetComponent<Collider>().bounds.size;
+        bounds[0] -= ((2 * wallOffset) - 2);
+        bounds[2] -= ((2 * wallOffset) - 2);
+
+        for (int count = 0; count < numObjects; count++)
+        {
+            float x = (float) (bounds[0] * rng.NextDouble() - bounds[0]/2);
+            float z = (float)(bounds[2] * rng.NextDouble() - bounds[2]/2);
+            Transform child = Instantiate(pickupPrefab, new Vector3(x, 0.5f, z), Quaternion.identity);
+            child.SetParent(this.gameObject.transform);
+        }
+
+        UpdateUIText();
+    }
+
+    public void UpdateUIText()
+    {
+        remainingPickupText.text = "Remaining objects: " + numObjects;
+    }
+
+    public void DestroyObject()
+    {
+        numObjects--;
+        UpdateUIText();
+    }
+
+    public bool HasRemainingObject()
+    {
+        return (numObjects > 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text winText;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        winText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        // Update Player Location (Physics)
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+
+            PickupController pickupController = other.gameObject.transform.parent.gameObject.GetComponent<PickupController>();
+            pickupController.DestroyObject();
+            if (!pickupController.HasRemainingObject())
+            {
+                winText.text = "You win!";
+            }
+        }
+    }
+}
